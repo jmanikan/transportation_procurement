81983486e4cb56673fd1fc8271e5f0cdf3717923
Juvenal Manikan <jmanikan@gmail.com>
2015-08-10 14:22:18 -0400

    Added Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   get 'users/new'
-
   root             'static_pages#home'
   # get 'static_pages/help'
   get 'help'    => 'static_pages#help'
@@ -9,9 +9,10 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   # get 'static_pages/contact'
   get 'contact' => 'static_pages#contact'
-
   get 'signup'  => 'users#new'
-
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'login' => 'sessions#destroy'
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
