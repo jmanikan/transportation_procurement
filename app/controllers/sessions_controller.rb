550d7086ef8fe67bdebe41b25728e2148ac6199c
Juvenal Manikan <jmanikan@gmail.com>
2015-08-11 23:26:33 -0400

    Added bootstrap to application.js and updated _header partial w/ logout links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,14 +1,15 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # Logs the user in and redirect to user show page
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      # flash.now only shows message once, instead of being rendered twice
       render 'new'
     end
   end
