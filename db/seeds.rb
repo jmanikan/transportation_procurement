2dea5e7d513641be34e8bfa6480c9f9aa7ef4c73
Juvenal Manikan <jmanikan@gmail.com>
2015-08-19 01:45:52 -0400

    Attempting to seed w/ faker gem; does not work

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,39 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = [
-  {
-    company: "Christopher Booty",
-    email: "chris@example.com",
-    password: "password",
-    password_confirmation: "password"
-  },
-  {
-    company: "Paul Bamberg",
-    email: "paul@example.com",
-    password: "password",
-    password_confirmation: "password"
-  }
-]
+# user = [
+#   {
+#     company: "Christopher Booty",
+#     email: "chris@example.com",
+#     password: "password",
+#     password_confirmation: "password"
+#   },
+#   {
+#     company: "Paul Bamberg",
+#     email: "paul@example.com",
+#     password: "password",
+#     password_confirmation: "password"
+#   }
+# ]
+#
+# users.each do |user|
+#   User.create user
+# end
 
-users.each do |user|
-  User.create user
+User.create!(company:  "Example Carrier",
+             email: "sample_carrier@example.com",
+             password:              "password",
+             password_confirmation: "password",
+             role: "carrier")
+
+99.times do |n|
+  company  = Faker::Company.company
+  email = "sample-#{n+1}@example.com"
+  password = "password"
+  role = "carrier"
+  User.create!(company: company,
+               email: email,
+               password:              password,
+               password_confirmation: password,
+               role: role)
 end
